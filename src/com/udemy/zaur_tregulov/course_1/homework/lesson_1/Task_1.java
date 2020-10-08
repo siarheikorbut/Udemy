e30b41fe2839f751d9459bc2ea47b3878e80e988
Сергей Корбут <siarheikorbut@gmail.com>
2020-10-08 18:09:11 +0300

    Initial commit

---
+++ src/com/udemy/zaur_tregulov/course_1/homework/lesson_1/Task_1.java
@@ -0,0 +1,13 @@
+package com.udemy.zaur_tregulov.course_1.homework.lesson_1;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        //Вывод текста в консоль с соблюдением необходимых отступов.
+        System.out.println("                  РУБАИ");
+        System.out.println("Много лет размышлял я над жизнью земной.");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда, открытая мной.");
+        System.out.println("                                 О. Хайям");
+    }
+}
---
+++ src/com/udemy/zaur_tregulov/course_2/homework/lesson_1/Task_1.java
@@ -0,0 +1,7 @@
+package com.udemy.zaur_tregulov.course_2.homework.lesson_1;
+
+public class Task_1 {
+    public static void main(String[] args) {
+
+    }
+}
