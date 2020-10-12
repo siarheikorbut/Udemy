60c6ca1b265aaa8f0337e53204de22aca8ae83c4
Сергей Корбут <siarheikorbut@gmail.com>
2020-10-12 17:41:09 +0300

    Udemy - Zaur Tregulov (Course 1/lesson 5/task 2) - completed.

--- src/com/udemy/zaur_tregulov/course_1/homework/lesson_5/BankAccount.java
+++ src/com/udemy/zaur_tregulov/course_1/homework/lesson_5/BankAccount.java
@@ -1,5 +1,15 @@
 package com.udemy.zaur_tregulov.course_1.homework.lesson_5;
 
+/**
+ * @author Siarhei Korbut
+ * Создать 2 класса. Первый класс с названием BankAccount, должен содержать в себе ID клиента, имя, фамилию и
+ * баланс счёта и 2 метода. Первый метод, с названием popolnenieScheta, должен увеличивать баланс на сумму
+ * указанную в параметрах этого метода. Второй метод, с названием snytieSoScheta, должен уменьшать
+ * баланс на сумму указанную в параметрах этого метода. Второй класс с названием BankAccountTest, должен
+ * создавать экземпляр класса BankAccount и вызывать созданные в нем методы, продемонстрировав результат их
+ * работы.
+ */
+
 public class BankAccount {
 
     int id;
@@ -15,12 +25,16 @@ public class BankAccount {
     }
 
 
-    void popolnenieScheta(double summaPopolneniya) {
+    public void popolnenieScheta(double summaPopolneniya) {
         System.out.printf("ID клиента: %d\nИмя: %s\nФамилия: %s\nБаланс: %.2f;\nСумма пополнения: %.2f\nБаланс после поплнения: %.2f\n\n", id, name, surname, balance, summaPopolneniya, balance + summaPopolneniya);
     }
 
-    void snytieSoScheta(double summaSnytiya) {
-        System.out.printf("ID клиента: %d\nИмя: %s\nФамилия: %s\nБаланс: %.2f;\nСумма снятия: %.2f\nБаланс после снятия: %.2f;\n\n", id, name, surname, balance, summaSnytiya, balance - summaSnytiya);
+    public void snytieSoScheta(double summaSnytiya) {
+        if (balance - summaSnytiya < 0) {
+            System.out.printf("ID клиента: %d\nИмя: %s\nФамилия: %s\nБаланс: %.2f;\nСумма снятия: %.2f\n\nНедостаточно средств!\n\n", id, name, surname, balance, summaSnytiya);
+        } else {
+            System.out.printf("ID клиента: %d\nИмя: %s\nФамилия: %s\nБаланс: %.2f;\nСумма снятия: %.2f\nБаланс после снятия: %.2f;\n\n", id, name, surname, balance, summaSnytiya, balance - summaSnytiya);
+        }
     }
 }
 
@@ -28,8 +42,6 @@ class BankAccountTest {
     public static void main(String[] args) {
         BankAccount user1 = new BankAccount(1, "Иван", "Иванов", 100);
         user1.popolnenieScheta(300);
-
-        BankAccount user2 = new BankAccount(2, "Петр","Петров", 2466);
-        user2.snytieSoScheta(450);
+        user1.snytieSoScheta(450);
     }
 }
