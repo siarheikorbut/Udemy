3238a4e0820477a89fbd5db4719616b0740d4958
Сергей Корбут <siarheikorbut@gmail.com>
2020-10-17 17:39:32 +0300

    Udemy - Zaur Tregulov (Course 1/lesson 11/task 1) - completed.

--- src/com/udemy/zaur_tregulov/course_1/homework/lesson_11/Car.java
+++ src/com/udemy/zaur_tregulov/course_1/homework/lesson_11/Car.java
@@ -1,4 +1,21 @@
 package com.udemy.zaur_tregulov.course_1.homework.lesson_11;
 
 public class Car {
+    String color;
+    String engine;
+    int doorCount;
+
+    public Car(String color, String engine, int doorCount) {
+        this.color = color;
+        this.engine = engine;
+        this.doorCount = doorCount;
+    }
+}
+
+class CarTest {
+    void changeDoorCount(int newDoorCount ) {
+
+        Car car1 = new Car("Черный", "V8", 2);
+
+    }
 }
