da6e5f8d0598320043e7878c33715a7d7bee0cfd
siarheikorbut <siarheikorbut@gmail.com>
2020-10-19 18:21:26 +0300

    Udemy - Zaur Tregulov (Course 1/lesson 15/task 1) - completed.

--- src/com/udemy/zaur_tregulov/course_1/homework/lesson_15/Task_1.java
+++ src/com/udemy/zaur_tregulov/course_1/homework/lesson_15/Task_1.java
@@ -1,11 +1,12 @@
-package com.udemy.zaur_tregulov.course_1.homework.lesson_14;
+package com.udemy.zaur_tregulov.course_1.homework.lesson_15;
 
 /**
  * @author Siarhei Korbut
  * 1. Создать класс с одним статическим методом, который должен выводить в консоль
  * время в формате "час:минута:секунда", в интервале от 0 до 6 часов. Если час
  * больше единицы и минута кратна 10, то метод необходимо закончить. Если секунда
- * умноженная на час больше минуты, то пора переходить на другую минуту.
+ * умноженная на час больше минуты, то пора переходить на другую минуту. Использовать
+ * циклы while для внешнего и внутренних циклов и do while для среднего цикла.
  * 2. Продемонстрировать работу созданного метода.
  */
 
@@ -13,21 +14,27 @@ public class Task_1 {
 
     //Вывод времени в консоль в заданном интервале и формате.
     static void timeInfo() {
+        int hour = 0;
         OUTER:
-        for (int hour = 0; hour < 6; hour++) {
+        while (hour < 6) {
+            int minute = -1;
             MIDDLE:
-            for (int minute = 0; minute < 60; minute++) {
+            do {
+                minute++;
                 if (hour > 1 && minute % 10 == 0) {
                     break OUTER;
                 }
+                int second = 0;
                 INNER:
-                for (int second = 0; second < 60; second++) {
+                while (second < 60) {
                     if (second * hour > minute) {
                         continue MIDDLE;
                     }
                     System.out.println(hour + ":" + minute + ":" + second);
+                    second++;
                 }
-            }
+            } while (minute < 59);
+            hour++;
         }
     }
 
