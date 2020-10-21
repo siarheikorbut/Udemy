ee6fceecbf9510f4db02dc4866b0d41263f0618a
siarheikorbut <siarheikorbut@gmail.com>
2020-10-21 15:48:39 +0300

    Udemy - Zaur Tregulov (Course 1/lesson 16/task 1) - completed.

--- src/com/udemy/zaur_tregulov/course_1/homework/lesson_16/Task_1.java
+++ src/com/udemy/zaur_tregulov/course_1/homework/lesson_16/Task_1.java
@@ -2,7 +2,7 @@ package com.udemy.zaur_tregulov.course_1.homework.lesson_16;
 
 /**
  * @author Siarhei Korbut
- * 1. Создать класс с одиним методом, метод должен называться email и принимать
+ * 1. Создать класс с одиним методом, метод должен называться emailInfo и принимать
  * в параметре тип данных String один раз. Данный параметр должен содержать в себе
  * e-mail-ы в следующем виде: ya@yahoo.com; on@mail.ru; ona@gmail.com; т.е.
  * после каждого e-mail должен стоять знак препинания ";". Метод должен выводить
@@ -13,10 +13,15 @@ package com.udemy.zaur_tregulov.course_1.homework.lesson_16;
 
 public class Task_1 {
 
-    //Поиск в строке нужного участка и вывод в консоль.
-    void emailInfo(String email){
-
-    }
+    //Поиск в строке нужного участка и вывод его в консоль.
+    void emailInfo(String email) {
+        for (int i = 0; i < email.length() - 1; i++) {
+            int a = email.indexOf('@', i);
+            int b = email.indexOf('.', i);
+            i = email.indexOf(';', i);
+            System.out.println("Пользователь использует почту: "+ email.substring(a + 1, b));
+            }
+        }
 
     public static void main(String[] args) {
         Task_1 t = new Task_1();
