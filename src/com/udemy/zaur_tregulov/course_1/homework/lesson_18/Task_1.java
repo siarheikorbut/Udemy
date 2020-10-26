4d69831ae220c6f1090deaa4f0ea963364f6bb0c
siarheikorbut <siarheikorbut@gmail.com>
2020-10-26 13:04:09 +0300

    Udemy - Zaur Tregulov (Course 1/lesson 18/task 1) - completed.

--- src/com/udemy/zaur_tregulov/course_1/homework/lesson_18/Task_1.java
+++ src/com/udemy/zaur_tregulov/course_1/homework/lesson_18/Task_1.java
@@ -1,5 +1,28 @@
 package com.udemy.zaur_tregulov.course_1.homework.lesson_18;
 
-public class Task_1 {
+import java.util.Arrays;
 
+/**
+ * @author Siarhei Korbut
+ * 1. Создать класс с одиним методом, метод должен называться sorting (сортировка) и
+ * принимать в параметры одномерный массив типа int. Метод должен возвращать
+ * отсортированный по возрастанию массив.
+ * 2. Продемонстрировать работу созданного метода.
+ */
+
+public class Task_1 {
+    public static void sorting(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int)(Math.random()*15);
+        }
+        sorting(array);
+        for (int number : array) {
+            System.out.println(number);
+        }
+    }
 }
