efa2ce15d83401c6796aa9d0ca95906f3cda8a9e
siarheikorbut <siarheikorbut@gmail.com>
2020-11-09 18:59:27 +0300

    Udemy - Zaur Tregulov (Course 1/lesson 22/test 1, 2, 3, 4, 5) - completed.

---
+++ src/com/udemy/zaur_tregulov/course_1/homework/lesson_22/Test_1/p2/Y.java
@@ -0,0 +1,22 @@
+package com.udemy.zaur_tregulov.course_1.homework.lesson_22.Test_1.p2;
+
+import com.udemy.zaur_tregulov.course_1.homework.lesson_22.Test_1.p1.X;
+
+/**
+ * @author Siarhei Korbut
+ * 1. Каков будет результат компиляции и запуска класса Y?
+ */
+
+public class Y extends X {
+    public Y() {
+    }
+
+    public void abc() {
+        System.out.println('Y');
+    }
+
+    public static void main(String[] args) {
+        Y y = new Y();
+        y.abc();
+    }
+}
---
+++ src/com/udemy/zaur_tregulov/course_1/homework/lesson_22/Test_2/p1/X.java
@@ -0,0 +1,7 @@
+package com.udemy.zaur_tregulov.course_1.homework.lesson_22.Test_2.p1;
+
+public class X {
+    protected void abc() {
+        System.out.println('X');
+    }
+}
