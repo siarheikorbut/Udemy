5bae45fa41496c8eb1faac06bed7ffe39c64df86
siarheikorbut <siarheikorbut@gmail.com>
2020-11-11 17:44:40 +0300

    Udemy - Zaur Tregulov (Course 1/lesson 23/task 1) - completed.

---
+++ src/com/udemy/zaur_tregulov/course_1/homework/lesson_23/Lion.java
@@ -0,0 +1,23 @@
+package com.udemy.zaur_tregulov.course_1.homework.lesson_23;
+
+public class Lion extends Mammal {
+    public Lion(String name) {
+        super(name);
+        this.name = name;
+    }
+
+    @Override
+    void run() {
+        System.out.println("Лев - это не самая быстрая кошка!");
+    }
+
+    @Override
+    void eat() {
+        System.out.println("Лев, как любой хищник, любит мясо!");
+    }
+
+    @Override
+    void sleep() {
+        System.out.println("Большую часть дня, лев спит!");
+    }
+}
---
+++ src/com/udemy/zaur_tregulov/course_1/homework/lesson_23/Mammal.java
@@ -0,0 +1,16 @@
+package com.udemy.zaur_tregulov.course_1.homework.lesson_23;
+
+public abstract class Mammal extends Animal implements Speakable {
+    public Mammal(String name) {
+        super(name);
+        this.name = name;
+    }
+
+    abstract void run();
+
+    interface Speakable {
+        default void speak() {
+            System.out.println("Кто-то говорит!");
+        }
+    }
+}
